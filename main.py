0dc3a03aa26707b6bc92b81bd04dc677f6b95bce
Yiğit <83499976+yilmazyigit@users.noreply.github.com>
2022-07-19 22:56:41 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import cv2
+from tracker import *
+
+tracker = EuclideanDistTracker()
+
+cap = cv2.VideoCapture("https://601a5ce60b9a1.streamlock.net/hls/573.stream/playlist.m3u8")
+# cap = cv2.VideoCapture(0)
+# cap = cv2.VideoCapture("1.mp4")
+
+
+# Object detection from Stable camera
+object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+while True:
+    ret, frame = cap.read()
+    height, width, _ = frame.shape
+
+    roi = frame[140: 720,300: 800]
+
+    mask = object_detector.apply(roi)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    detections = []
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > 100:
+            x, y, w, h = cv2.boundingRect(cnt)
+            detections.append([x, y, w, h])
+
+    boxes_ids = tracker.update(detections)
+    for box_id in boxes_ids:
+        x, y, w, h, id = box_id
+        cv2.putText(roi, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+
+    cv2.imshow("roi", roi)
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mask", mask)
+
+    key = cv2.waitKey(30)
+    if key == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
